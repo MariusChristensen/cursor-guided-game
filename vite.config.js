89b000c36a28664311eb2e3b719a556f7aef1e10
MariusChristensen <91680871+MariusChristensen@users.noreply.github.com>
2024-12-17 11:35:17 +0100

    Fixed firebase deployment for good now I believe. Was some issue with how Vite compiled

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src",
-  base: "/cursor-guided-game/",
+  base: "./",
   build: {
     target: "esnext",
     outDir: "../docs", // Changed from '../dist' to '../docs' for GitHub Pages
