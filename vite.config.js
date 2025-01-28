7fe878ccbf671e2a55f0f4d97bc7d178ccb9ea78
MariusChristensen <91680871+MariusChristensen@users.noreply.github.com>
2025-01-28 11:47:38 +0100

    Switch to GitHub Actions deployment with environment variables

--- vite.config.js
+++ vite.config.js
@@ -9,17 +9,4 @@ export default defineConfig({
     emptyOutDir: true,
   },
   envDir: "../",
-  define: {
-    "import.meta.env.VITE_FIREBASE_API_KEY":
-      "'AIzaSyDi-8StlfNmKlZVPPhS8CKCjDCIqrJfNFc'",
-    "import.meta.env.VITE_FIREBASE_AUTH_DOMAIN":
-      "'snakeysnake-34426.firebaseapp.com'",
-    "import.meta.env.VITE_FIREBASE_PROJECT_ID": "'snakeysnake-34426'",
-    "import.meta.env.VITE_FIREBASE_STORAGE_BUCKET":
-      "'snakeysnake-34426.firebasestorage.app'",
-    "import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID": "'701773775110'",
-    "import.meta.env.VITE_FIREBASE_APP_ID":
-      "'1:701773775110:web:252b8dd41bcb28ea545e87'",
-    "import.meta.env.VITE_FIREBASE_MEASUREMENT_ID": "'G-S95YBN43ME'",
-  },
 });
